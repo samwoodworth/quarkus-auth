26c53e4a48999e03cb3040211da07c3fb911bc02
samwoodworth <swoodworth177@gmail.com>
2022-01-24 22:03:41 -0500

    Initial commit

---
+++ src/main/java/org/quarkus/auth/controller/AuthController.java
@@ -0,0 +1,6 @@
+package org.quarkus.auth.controller;
+
+public class AuthController {
+
+
+}
---
+++ src/main/java/org/quarkus/auth/entity/User.java
@@ -0,0 +1,12 @@
+package org.quarkus.auth.entity;
+
+public class User {
+
+
+    private Long userId;
+    private String userName;
+    private String password;
+    private boolean active;
+    private String roles;
+    private boolean loggedIn;
+}
